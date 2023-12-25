a004f7408ebaad54af1e7ef3b3ee0f04be46906d
Khiran Gannu <21bd1a056ncsed@gmail.com>
2023-12-25 17:15:49 +0530

    tenth commit

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,55 +1,16 @@
-// import UserCard from '@/components/UserCard/Usercard';
-// import styles from './page.module.css';
-// import { prisma } from '@/lib/prisma';
-
-// export default async function Users() {
-//   const users = await prisma.user.findMany();
-
-//   return (
-//     <div className={styles.grid}>
-//       {users.map((user) => {
-//         return <UserCard key={user.id} {...user} />;
-//       })}
-//     </div>
-//   );
-// }
-
-
-// app/users/page.tsx
-
 import UserCard from '@/components/UserCard/Usercard';
 import styles from './page.module.css';
 import { prisma } from '@/lib/prisma';
 
-interface User {
-  id: string;
-  name?: string | null | undefined;
-  age?: number | null | undefined;
-  image?: string | null | undefined;
-}
+export default async function Users() {
+  const users = await prisma.user.findMany();
 
-const Users = ({ users }: { users: User[] }) => {
   return (
     <div className={styles.grid}>
-      {users && users.map((user) => (
-        <UserCard key={user.id} {...user} />
-      ))}
+      {users.map((user) => {
+        return <UserCard key={user.id} {...user} />;
+      })}
     </div>
   );
-};
-
-export async function getServerSideProps() {
-  try {
-    const users = await prisma.user.findMany();
-    return {
-      props: { users },
-    };
-  } catch (error) {
-    console.error('Error fetching users:', error);
-    return {
-      props: { users: [] },
-    };
-  }
 }
 
-export default Users;
