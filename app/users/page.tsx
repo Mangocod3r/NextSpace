decd87658dfa44d025e3a5733f024154bffa0af8
Khiran Gannu <21bd1a056ncsed@gmail.com>
2023-12-25 16:42:19 +0530

    tenth commit

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,15 +1,49 @@
+// import UserCard from '@/components/UserCard/Usercard';
+// import styles from './page.module.css';
+// import { prisma } from '@/lib/prisma';
+
+// export default async function Users() {
+//   const users = await prisma.user.findMany();
+
+//   return (
+//     <div className={styles.grid}>
+//       {users.map((user) => {
+//         return <UserCard key={user.id} {...user} />;
+//       })}
+//     </div>
+//   );
+// }
+// Assuming a generic user type, update this according to your user structure
+type User = {
+  id: string,
+  name: string,
+  age: number,
+  image: string;
+  // Other user properties...
+};
+
 import UserCard from '@/components/UserCard/Usercard';
 import styles from './page.module.css';
 import { prisma } from '@/lib/prisma';
 
-export default async function Users() {
+interface UsersProps {
+  users: User[]; // Specify the type of the 'users' prop
+}
+
+export async function getServerSideProps() {
   const users = await prisma.user.findMany();
 
+  return {
+    props: { users },
+  };
+}
+
+export default function Users({ users }: UsersProps) {
   return (
     <div className={styles.grid}>
-      {users.map((user) => {
-        return <UserCard key={user.id} {...user} />;
-      })}
+      {users.map((user) => (
+        <UserCard key={user.id} {...user} />
+      ))}
     </div>
   );
 }
